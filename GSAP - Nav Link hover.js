6388bca4665511f29ca02f7f86e8f9edf3aa6f49
uncodestudio <140694478+uncodestudio@users.noreply.github.com>
2024-11-04 15:06:34 +0400

    Updating GSAP - Nav Link hover

---
+++ GSAP - Nav Link hover.js
@@ -0,0 +1,31 @@
+document.querySelectorAll('.navbar_link').forEach(link => {
+  const menuSelect = link.querySelector('.menu_select');
+
+  link.addEventListener('mouseenter', () => {
+    gsap.to(link, {
+      duration: 0.3, // Durée de l'animation en secondes
+      x: 20, // Translation en X de 20px
+      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // Type d'animation
+    });
+
+    gsap.to(menuSelect, {
+      duration: 0.3, // Durée de l'animation en secondes
+      scale: 1, // Passage à une échelle de 1
+      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // Type d'animation
+    });
+  });
+
+  link.addEventListener('mouseleave', () => {
+    gsap.to(link, {
+      duration: 0.3, // Durée de l'animation en secondes
+      x: 0, // Réinitialiser la translation en X à 0
+      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // Type d'animation
+    });
+
+    gsap.to(menuSelect, {
+      duration: 0.3, // Durée de l'animation en secondes
+      scale: 0, // Passage à une échelle de 0
+      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // Type d'animation
+    });
+  });
+});
