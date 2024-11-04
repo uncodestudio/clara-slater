1630c5564840b5e59e9c22d8895cddc3e6b91d4f
uncodestudio <140694478+uncodestudio@users.noreply.github.com>
2024-11-04 15:25:23 +0400

    Updating GSAP - Nav Link hover

--- GSAP - Nav Link hover.js
+++ GSAP - Nav Link hover.js
@@ -5,13 +5,13 @@ document.querySelectorAll('.navbar_link').forEach(link => {
     gsap.to(link, {
       duration: 0.3, // Durée de l'animation en secondes
       x: 20, // Translation en X de 20px
-      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // Type d'animation
+      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // animation easinganimation easing
     });
 
     gsap.to(menuSelect, {
       duration: 0.3, // Durée de l'animation en secondes
       scale: 1, // Passage à une échelle de 1
-      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // Type d'animation
+      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // animation easinganimation easinganimation easing
     });
   });
 
@@ -19,13 +19,13 @@ document.querySelectorAll('.navbar_link').forEach(link => {
     gsap.to(link, {
       duration: 0.3, // Durée de l'animation en secondes
       x: 0, // Réinitialiser la translation en X à 0
-      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // Type d'animation
+      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // animation easing
     });
 
     gsap.to(menuSelect, {
       duration: 0.3, // Durée de l'animation en secondes
       scale: 0, // Passage à une échelle de 0
-      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // Type d'animation
+      ease: 'cubic-bezier(0.64, 0, 0.25, 1)' // animation easing
     });
   });
 });
