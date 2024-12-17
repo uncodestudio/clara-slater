fa69c27ddaecd9f951be9532278b919a5aa57334
uncodestudio <140694478+uncodestudio@users.noreply.github.com>
2024-12-17 15:58:55 +0400

    Updating GSAP Animations - Articles de blog

---
+++ GSAP Animations - Articles de blog.js
@@ -0,0 +1,21 @@
+//Texte animé DONE
+let typeSplit = new SplitType('[animate]', {
+  types: 'lines, words, chars',
+  tagName: 'span'
+})
+
+gsap.from('[animate] .word', {
+  opacity: 0.2,
+  duration: 0.5,
+  ease: 'power1.out',
+  stagger: 0.1,
+  markers: false,
+
+  scrollTrigger: {
+    trigger: '[animate]',
+    start: 'top 75%',
+    end: 'top 35%',
+    scrub: true,
+    markers: false,
+  }
+})
